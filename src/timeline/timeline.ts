09446da4857d954fcd4c49c8d4cd48b0f0074cf1
212461381 <ambyr-shae.jarrell@ge.com>
2017-12-01 16:37:36 -0600

    Stylized timeline chart
    Removed x and y axis labels
    Aligned emojis with chart
    Changed color of home button and emojis

--- src/timeline/timeline.ts
+++ src/timeline/timeline.ts
@@ -1,21 +1,45 @@
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
-
 @Component({
-  selector: 'timeline',
-  templateUrl: 'timeline.html',
-  styleUrls: ['timeline.css']
+    selector: 'timeline',
+    templateUrl: 'timeline.html',
+    styleUrls: ['timeline.css']
 })
 
 export class Timeline {
 
-    name: string;
-    labels: string[] = ['a', 'b', 'c'];
-    data: number[] = [5, 3, -5];
-    type: string = 'line';
+    yAxis: string[] = ['a', 'b', 'c', 'd', 'e', 'f'];
+    xAxis: Array<{data: number[], label: string}> = [{
+            data: [5, 3, -5, 1, 0, 3],
+            label: '' // may need to remove this
+        }];
+    chartType: string = 'line';
+    chartOptions: any = {
+        legend: {
+            display: false,
+        },
+        scales: {
+            yAxes: [{
+                gridLines: {
+                    display: false
+                },
+                ticks: {
+                    fontColor: 'white'
+                }
+            }],
+            xAxes: [{
+                gridLines: {
+                    display: false
+                },
+                ticks: {
+                    fontColor: 'white'
+                }
+            }],
+        }
+    };
 
-    constructor(private router: Router) {}
+    constructor(private router: Router) { }
 
     goHome() {
         this.router.navigateByUrl('../home');
