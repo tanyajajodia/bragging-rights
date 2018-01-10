6bbc038e12ebf98324fc83a905c634feb63d36b4
212461381 <ambyr-shae.jarrell@ge.com>
2018-01-09 19:46:54 -0600

    Created title component
    The title component appears on multiple pages.

---
+++ src/components/title/title.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'title',
+    templateUrl: 'title.html',
+    styleUrls: [ 'title.css' ]
+})
+
+export class Title {
+    constructor() {}
+}
