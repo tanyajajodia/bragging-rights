99ba9b99db3ca82d98135fe96903340265bc5751
212461381 <ambyr-shae.jarrell@ge.com>
2018-03-02 14:54:51 -0600

    Added text area
    Added text area to input reason for nomination.
    Styled text area.
    Refactored onFocus and onBlur to include text area.

--- src/components/join-the-brag/join-the-brag.ts
+++ src/components/join-the-brag/join-the-brag.ts
@@ -35,16 +35,26 @@ export class Join implements AfterViewInit {
     }
 
     onFocus(event) {
-        event.target.parentNode.classList.add('mdc-text-field--focused');
-        event.target.parentNode.classList.add('mdc-text-field-focus');
-        event.target.nextElementSibling.classList.add('mdc-text-field__label--float-above');
+        const target = event.target;
+        const parent = target.parentNode;
+
+        parent.classList.add('mdc-text-field--focused');
+        parent.classList.add('mdc-text-field-focus');
+
+        if (target.nextElementSibling !== null && target.nextElementSibling.nodeName === 'SPAN') {
+            target.nextElementSibling.classList.add('mdc-text-field__label--float-above');
+        }
     }
 
     onBlur(event) {
-        event.target.parentNode.classList.remove('mdc-text-field--focused');
-        event.target.parentNode.classList.remove('mdc-text-field-focus');
-        if (event.target.value === '') {
-            event.target.nextElementSibling.classList.remove('mdc-text-field__label--float-above');
+        const target = event.target;
+        const parent = target.parentNode;
+
+        parent.classList.remove('mdc-text-field--focused');
+        parent.classList.remove('mdc-text-field-focus');
+
+        if (target.nextElementSibling !== null && target.nextElementSibling.nodeName === 'SPAN' && target.value === '') {
+            target.nextElementSibling.classList.remove('mdc-text-field__label--float-above');
         }
     }
 
