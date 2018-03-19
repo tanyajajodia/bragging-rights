8cdd4d705dc71ab33683274f22cb61ae7b61791c
212461381 <ambyr-shae.jarrell@ge.com>
2018-03-19 12:12:37 -0500

    Added Elise. Removed menu items

--- src/components/menu/menu.ts
+++ src/components/menu/menu.ts
@@ -12,29 +12,14 @@ export class Menu {
 
     constructor( private router: Router ) {}
 
-    // navigate to home page
-    goHome() {
-        this.router.navigateByUrl('home');
-    }
-
-    // navigate to sessions page
-    goToSessions() {
-        this.router.navigateByUrl('sessions');
-    }
-
     // navigate to braggers page
     goToBraggers() {
         this.router.navigateByUrl('braggers');
     }
 
     // navigate to become a bragger page
-    goToBecome() {
-        this.router.navigateByUrl('become-a-bragger');
-    }
-
-    // navigate to suggest a bragger page
-    goToSuggest() {
-        // this.router.navigateByUrl('suggest-a-bragger');
+    goToJoin() {
+        this.router.navigateByUrl('join-the-brag');
     }
 
     isActive( id: string ) {
